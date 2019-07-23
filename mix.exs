a5954e75d3da0bc9c4a9008c8f6e8395dbfc1bfb
Ivan Ivchenko <waltereevan@gmail.com>
2019-07-23 11:04:04 +0600

    Mix update

--- mix.exs
+++ mix.exs
@@ -1,16 +1,23 @@
 defmodule ExVerticalBooking.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :ex_vertical_booking,
-      version: "0.1.0",
+      version: @version,
       build_path: "./_build",
       config_path: "./config/config.exs",
       deps_path: "./deps",
       lockfile: "./mix.lock",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      name: "ExVerticalBooking",
+      source_url: "https://github.com/ChannexIO/ex_vertical_booking",
+      docs: docs(),
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -30,7 +37,26 @@ defmodule ExVerticalBooking.MixProject do
       {:httpoison, "~> 1.3"},
       {:elixir_uuid, "~> 1.2"},
       {:exsom, "~> 0.1.0"},
-      {:detergentex, "0.0.5"}
+      {:detergentex, "0.0.5"},
+      {:ex_doc, "~> 0.21", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [extras: ["README.md"]]
+  end
+
+  defp description do
+    "VerticalBooking API Extension for Elixir"
+  end
+
+  defp package do
+    [
+      name: "ex_vertical_booking",
+      files: ~w(lib config .formatter.exs mix.exs README.md LICENSE),
+      maintainers: ["Andrew Judis Yudin"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/ChannexIO/ex_vertical_booking"}
     ]
   end
 end
