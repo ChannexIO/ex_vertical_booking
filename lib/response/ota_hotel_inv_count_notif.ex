29c1ca911416c52489c7dcce431a9963187f66e9
EevanW <32407548+EevanW@users.noreply.github.com>
2019-09-25 00:18:07 +0600

    Formatting CNX-167

--- lib/response/ota_hotel_inv_count_notif.ex
+++ lib/response/ota_hotel_inv_count_notif.ex
@@ -1,8 +1,7 @@
 defmodule ExVerticalBooking.Response.OtaHotelInvCountNotif do
   alias ExVerticalBooking.Response.Converter
 
-  @list_nodes [
-  ]
+  @list_nodes []
 
   def list_nodes, do: @list_nodes
   def convert(struct), do: Converter.convert(struct, @list_nodes)
