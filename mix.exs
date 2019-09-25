57ac2d00b400f8197fbac94d61e69e50e0f55ef1
Ivan Ivchenko <waltereevan@gmail.com>
2019-09-25 12:32:08 +0600

    Change elixir version CNX-167

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule ExVerticalBooking.MixProject do
       config_path: "./config/config.exs",
       deps_path: "./deps",
       lockfile: "./mix.lock",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       name: "ExVerticalBooking",
       source_url: "https://github.com/ChannexIO/ex_vertical_booking",
