8ed75c363e4021a35af5e0f08c82c20ae4d29c11
Ivan Ivchenko <waltereevan@gmail.com>
2019-09-25 12:14:54 +0600

    Expand exceptions CNX-167

--- lib/response/fault_processor.ex
+++ lib/response/fault_processor.ex
@@ -8,12 +8,13 @@ defmodule ExVerticalBooking.Response.FaultProcessor do
       meta |> Map.put(:success, false) |> Map.put(:errors, [Error.message(e) | meta.errors])}
   end
 
-  def convert(%{faultcode: code, faultstring: _string}), do: Error.exception(code)
-  def convert(%{"faultcode" => code, "faultstring" => _string}), do: Error.exception(code)
+  def convert(%{faultcode: code, faultstring: _string}), do: Error.exception({:http_error, {code, string}})
+  def convert(%{"faultcode" => code, "faultstring" => string}), do: Error.exception({:http_error, {code, string}})
   def convert({:fatal, reason}), do: Error.exception({:fatal, reason})
   def convert({:exit, reason}), do: Error.exception({:exit, reason})
   def convert({:badrpc, {_, reason}}), do: Error.exception(reason)
   def convert({:error, _, %{errors: [reason]}}), do: Error.exception(reason)
   def convert(%ArgumentError{} = reason), do: Error.exception({:argument_error, reason})
   def convert(%FunctionClauseError{} = reason), do: Error.exception({:function_clause, reason})
+  def convert(reason), do: Error.exception(reason)
 end
